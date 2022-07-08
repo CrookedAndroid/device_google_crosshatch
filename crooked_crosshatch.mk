75f85373db1c303da1dfb22bd3dfbb36d7732d11
neporood <neporood@gmail.com>
2022-07-07 22:08:43 -0500

    b1c1: update build & desc to June 2022 (SP1A.210812.016.C2)
    - reorder device identifiers
    - change blueline to use crosshatch vendor

--- crooked_crosshatch.mk
+++ crooked_crosshatch.mk
@@ -13,9 +13,10 @@ $(call inherit-product, device/google/crosshatch/aosp_crosshatch.mk)
 include device/google/crosshatch/crosshatch/device-crooked.mk
 
 # Device identifier. This must come after all inclusions
+PRODUCT_NAME := crooked_crosshatch
+PRODUCT_DEVICE := crosshatch
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 3 XL
-PRODUCT_NAME := crooked_crosshatch
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2960
@@ -23,9 +24,9 @@ TARGET_SCREEN_WIDTH := 1440
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=crosshatch \
-    PRIVATE_BUILD_DESC="crosshatch-user 12 SP1A.210812.016.C1 8029091 release-keys"
+    PRIVATE_BUILD_DESC="crosshatch-user 12 SP1A.210812.016.C2 8618562 release-keys"
 
-BUILD_FINGERPRINT := google/crosshatch/crosshatch:12/SP1A.210812.016.C1/8029091:user/release-keys
+BUILD_FINGERPRINT := google/crosshatch/crosshatch:12/SP1A.210812.016.C2/8618562:user/release-keys
 
 $(call inherit-product, vendor/google/crosshatch/crosshatch-vendor.mk)
 $(call inherit-product-if-exists, vendor/gms/products/gms.mk)
