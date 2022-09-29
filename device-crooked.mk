57f629583ae96fecd6459de79332216aeb53b44a
Danny Lin <danny@kdrag0n.dev>
2022-09-28 20:45:19 -0500

    b1c1: Build Active Edge service (ElmyraService)
    This package provides comprehensive support for the Active Edge squeeze
    gesture supported on this device.
    
    Change-Id: I2158c22e727e293e3f02b9ddf614a943848ad587

--- device-crooked.mk
+++ device-crooked.mk
@@ -5,6 +5,10 @@
 
 DEVICE_PACKAGE_OVERLAYS += device/google/crosshatch/overlay-crooked
 
+# Elmyra
+PRODUCT_PACKAGES += \
+    ElmyraService
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
